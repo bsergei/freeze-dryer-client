79ce129ad0b21a90918f857fed925ea45aacbb23
S <bubenka@gmail.com>
2018-08-26 19:09:02 +0200

    Added reboot component and button

--- src/app/reboot/reboot.component.ts
+++ src/app/reboot/reboot.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Api } from '../services/api';
 import { timer, Subscription } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
@@ -9,31 +9,40 @@ import { Router } from '@angular/router';
   templateUrl: './reboot.component.html',
   styleUrls: ['./reboot.component.css']
 })
-export class RebootComponent implements OnInit {
+export class RebootComponent implements OnInit, OnDestroy {
 
   private subscription: Subscription;
+  private subscriptionT: Subscription;
+  private t: number;
 
   constructor(
     private api: Api,
-    private router: Router
-  ) {
+    private router: Router) {
   }
 
   ngOnInit() {
+    this.t = 0;
     this.subscription = timer(10000, 10000)
-      .pipe(switchMap(_ => {
+      .subscribe(async () => {
         try {
-          const res = this.api.getSensorsStatus();
+          const res = await this.api.getSensorsStatus().toPromise();
+          this.subscription.unsubscribe();
+          this.router.navigate(['dashboard']);
           return res;
         } catch (e) {
           return undefined;
         }
-      }))
-      .subscribe(r => {
-        if (r) {
-          this.subscription.unsubscribe();
-          this.router.navigate(['dashboard']);
-        }
       });
+
+      this.subscriptionT = timer(1000, 1000)
+        .subscribe(() => {
+          this.t++;
+        });
+  }
+
+  public ngOnDestroy(): void {
+    if (this.subscriptionT) {
+      this.subscriptionT.unsubscribe();
+    }
   }
 }
